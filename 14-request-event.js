2f32b71558f296b3fb79d28c167036b9be8f1779
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-14 16:56:28 +0100

    Example showing that servers are children of event emitters

---
+++ 14-request-event.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+// Server is a child of event emitter. so can use on and user server just like event emitters
+
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+const server = http.createServer()
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+  res.end('Welcome')
+})
+
+server.listen(5000)
